571a6ce9ec12523291089b68d8fc7a3e126d9748
Damaris Ngari <wanjirungari2@gmail.com>
2025-02-21 01:12:53 +0300

    car.py

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,10 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_number, wheel_size):
+        super().__init__(wheel_number, wheel_size)
+        self.horsepower = 400
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
